ae78db4f1bb244c0860bde8ee7860fd1edb59f5b
D <deepak.poosa95@gmail.com>
2018-09-14 22:16:44 +0530

    angularjs project

--- TodoList/client/src/app/todo/todo.component.ts
+++ TodoList/client/src/app/todo/todo.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
- items = new Array();
+ //items = new Array();
+ items = [];
   constructor() {
     
    }
